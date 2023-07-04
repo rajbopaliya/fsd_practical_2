d1f25c4bca271676f92a54d71d4f8c142b4b8ca9
rajbopaliya <d22dcs149@charusat.edu.in>
2023-07-04 19:15:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './components/home/Home';  
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import { BrowserRouter, Route,Routes } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/" element={<Home />}></Route>
+      <Route exact path="/about" element={<About />}></Route>
+      <Route exact path="/contact" element={<Contact />}></Route>
+      
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function Contact(){
+    return <h2>CONTACT</h2>
+}
