6b0d1d0e8c409f5ec5457e480b778c2acd1c8f78
rajbopaliya <132642006+rajbopaliya@users.noreply.github.com>
2024-02-24 12:31:26 +0530

    Delete src directory

--- src/components/about/About.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-
-export default function About(){
-    return <h2>ABOUT</h2>
-}
--- src/components/contact/Contact.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-
-export default function Contact(){
-    return <h2>CONTACT</h2>
-}
